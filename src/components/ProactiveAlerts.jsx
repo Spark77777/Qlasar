4ab902d56cb192dd0f7916e2f180ebf903fc5864
Spark77777 <dheerajss348@gmail.com>
2025-10-01 19:48:52 +0530

    Delete src/components/ProactiveAlerts.jsx

--- src/components/ProactiveAlerts.jsx
+++
@@ -1,8 +0,0 @@
-export default function ProactiveAlerts() {
-  return (
-    <div className="w-1/4 bg-gray-50 p-4 border-l">
-      <h2 className="text-lg font-bold mb-4">Proactive Alerts</h2>
-      {/* Alerts will populate here */}
-    </div>
-  );
-}
