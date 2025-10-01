d1c7f31e0f62268a2fc8fa30bce4464bed754375
Spark77777 <dheerajss348@gmail.com>
2025-10-01 19:48:41 +0530

    Delete src/components/MessageCard.jsx

--- src/components/MessageCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function MessageCard({ message, type }) {
-  const alignment = type === "user" ? "text-right" : "text-left";
-  return (
-    <div className={`mb-2 p-2 rounded ${alignment} ${type === "user" ? "bg-teal-100" : "bg-gray-200"}`}>
-      {message}
-    </div>
-  );
-}
