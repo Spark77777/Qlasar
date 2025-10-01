8c8880c9b92905b73bb3c2ec5901c81c4271c656
Spark77777 <dheerajss348@gmail.com>
2025-10-01 19:49:08 +0530

    Delete src/components/SessionSidebar.jsx

--- src/components/SessionSidebar.jsx
+++
@@ -1,9 +0,0 @@
-export default function SessionSidebar() {
-  return (
-    <div className="w-1/5 bg-gray-100 p-4">
-      <h1 className="text-xl font-bold mb-4">Qlasar Sessions</h1>
-      <button className="mb-4 bg-teal-500 text-white px-4 py-2 rounded">+ New Session</button>
-      {/* List of sessions will go here */}
-    </div>
-  );
-}
