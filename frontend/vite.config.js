d76b1f5f7b1813f1153ccc4cbd19656b1744c5e5
Spark77777 <dheerajss348@gmail.com>
2025-10-02 10:51:39 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: "./",
   build: {
-    outDir: "../backend/static",
-    emptyOutDir: true,
-  },
+    outDir: "../backend/static"
+  }
 });
