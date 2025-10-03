39fc8521763920b2b952029336318432c2d05084
Spark77777 <dheerajss348@gmail.com>
2025-10-03 13:26:17 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import SessionSidebar from "./components/SessionSidebar";
 import ChatWindow from "./components/ChatWindow";
 import ProactiveAlerts from "./components/ProactiveAlerts";
@@ -6,26 +6,13 @@ import ProactiveAlerts from "./components/ProactiveAlerts";
 export default function App() {
   const [showAlerts, setShowAlerts] = useState(false);
 
-  const toggleAlerts = () => {
-    setShowAlerts((prev) => !prev);
-  };
-
   return (
-    <div className="flex h-screen w-screen overflow-hidden bg-gray-50">
-      {/* Left Sidebar */}
+    <div className="flex h-screen">
       <SessionSidebar />
-
-      {/* Center Chat Window */}
-      <div className={`flex-1 transition-all duration-300 ${showAlerts ? "md:w-2/3" : "w-full"}`}>
-        <ChatWindow onToggleAlerts={toggleAlerts} alertsVisible={showAlerts} />
+      <div className="flex-1 flex flex-col">
+        <ChatWindow toggleAlerts={() => setShowAlerts(!showAlerts)} />
       </div>
-
-      {/* Right Panel (Proactive Alerts) */}
-      {showAlerts && (
-        <div className="w-1/3 border-l border-gray-200 bg-white transition-all duration-300">
-          <ProactiveAlerts />
-        </div>
-      )}
+      {showAlerts && <ProactiveAlerts />}
     </div>
   );
 }
