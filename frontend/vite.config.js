99ffd2aa469f29531f4332055b95b5097a8bcaeb
Spark77777 <dheerajss348@gmail.com>
2025-10-03 09:36:57 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, "../backend/static"), // Output build to backend/static
-    emptyOutDir: true, // Clear folder before build
-    rollupOptions: {
-      input: path.resolve(__dirname, "index.html")
-    }
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+  server: {
+    port: 3000,
   },
 });
