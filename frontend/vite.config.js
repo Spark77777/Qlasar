84b3dc639b2ca00cd6e7a981dd09a4c8389902a8
Spark77777 <dheerajss348@gmail.com>
2025-10-03 19:11:18 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/static/", // all asset URLs start with /static/
   build: {
-    outDir: "../backend/static", // output directly to backend static folder
-    emptyOutDir: true,            // clean the folder before build
+    outDir: '../backend/static', // output goes to backend static folder
+    emptyOutDir: true
   },
+  root: '.'
 });
