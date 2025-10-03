5b413d1356ca9441d4df02f64c3492c7ffbf3a94
Spark77777 <dheerajss348@gmail.com>
2025-10-03 09:40:03 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
-import ChatWindow from "./components/ChatWindow";
+import React from "react";
 import SessionSidebar from "./components/SessionSidebar";
+import ChatWindow from "./components/ChatWindow";
 import ProactiveAlerts from "./components/ProactiveAlerts";
 
 export default function App() {
-  const [activeSession, setActiveSession] = useState("General");
-  const [alertsTopic, setAlertsTopic] = useState("");
-
   return (
-    <div className="flex h-screen">
-      <SessionSidebar activeSession={activeSession} setActiveSession={setActiveSession} />
-      <ChatWindow activeSession={activeSession} />
-      <ProactiveAlerts topic={alertsTopic} setTopic={setAlertsTopic} />
+    <div className="h-screen grid grid-cols-12">
+      {/* Left Sidebar */}
+      <div className="col-span-2 bg-gray-900 text-white p-4">
+        <SessionSidebar />
+      </div>
+
+      {/* Chat Window */}
+      <div className="col-span-7 bg-white p-4 border-x border-gray-200">
+        <ChatWindow />
+      </div>
+
+      {/* Proactive Alerts */}
+      <div className="col-span-3 bg-gray-50 p-4">
+        <ProactiveAlerts />
+      </div>
     </div>
   );
 }
