95d3675206ecf30ed7a6cc193e96d285ec7c86c7
Spark77777 <dheerajss348@gmail.com>
2025-10-03 10:44:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: '.', // this makes Vite use the folder containing vite.config.js as root
   plugins: [react()],
-  root: "./frontend",
   build: {
-    outDir: path.resolve(__dirname, "../backend/static"),
-    emptyOutDir: true,
+    outDir: path.resolve(__dirname, '../backend/static'), // output goes to backend/static
+    emptyOutDir: true, // clears the folder before build
     rollupOptions: {
-      input: path.resolve(__dirname, "frontend/index.html"),
+      input: path.resolve(__dirname, 'index.html'), // explicitly tell Rollup where index.html is
     },
   },
 });
