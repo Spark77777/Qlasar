dfed72783e3abfc4602856c85aa209a7c1cf5d68
Spark77777 <dheerajss348@gmail.com>
2025-10-03 11:53:43 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname), // frontend folder
   plugins: [react()],
+  base: './',
   build: {
-    outDir: path.resolve(__dirname, '../backend/static'), // build output for FastAPI
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'), // ensure Rollup sees index.html
-    },
-  },
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 });
