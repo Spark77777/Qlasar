6d04b477c4e179c5de5978017cecf48770c932f2
Spark77777 <dheerajss348@gmail.com>
2025-10-03 10:21:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,14 @@
 import React from "react";
-import SessionSidebar from "./components/SessionSidebar";
 import ChatWindow from "./components/ChatWindow";
+import SessionSidebar from "./components/SessionSidebar";
 import ProactiveAlerts from "./components/ProactiveAlerts";
 
 export default function App() {
   return (
-    <div className="h-screen grid grid-cols-12">
-      {/* Left Sidebar */}
-      <div className="col-span-2 bg-gray-900 text-white p-4">
-        <SessionSidebar />
-      </div>
-
-      {/* Chat Window */}
-      <div className="col-span-7 bg-white p-4 border-x border-gray-200">
-        <ChatWindow />
-      </div>
-
-      {/* Proactive Alerts */}
-      <div className="col-span-3 bg-gray-50 p-4">
-        <ProactiveAlerts />
-      </div>
+    <div className="flex h-screen">
+      <SessionSidebar />
+      <ChatWindow />
+      <ProactiveAlerts />
     </div>
   );
 }
