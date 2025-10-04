88413348c07cd5c8c602c0c51fae96dc784d8b1d
Spark77777 <dheerajss348@gmail.com>
2025-10-04 11:30:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: ".", // frontend folder is the root
-  base: "./",
+  root: 'frontend',
   build: {
-    outDir: "dist",   // this ensures Netlify sees dist folder
+    outDir: '../backend/static',
     emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, "index.html"),
-    },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, './frontend/src'),
     },
   },
 });
