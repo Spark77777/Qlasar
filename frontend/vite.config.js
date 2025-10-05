ab4ef031bc854928745d0f319350dce03fc864dc
Spark77777 <dheerajss348@gmail.com>
2025-10-05 16:22:22 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: '.', // project root has index.html
   plugins: [react()],
   build: {
-    outDir: 'dist', // production build output
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+    outDir: "dist"
+  }
 });
