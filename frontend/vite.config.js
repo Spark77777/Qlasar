e220a8a6c5947206821e812b5de0890126d598dc
Spark77777 <dheerajss348@gmail.com>
2025-10-09 21:00:11 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ✅ crucial for relative paths in production
   build: {
     outDir: "dist",
   },
